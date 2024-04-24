4b3f6fb62bafecb60b05c7c9ec7088debef9ad02
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2024-04-24 17:27:44 -0300

    pick GITHUB_JOB env var only

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const process = require('process');
 
-// Function to print all environment variables containing "GIT"
 function printGitEnvVars() {
-  console.log("Environment variables containing 'GIT':");
   for (const [key, value] of Object.entries(process.env)) {
-    if (key.includes('GIT')) {
+    if (key.includes('GITHUB_STEP')) {
       console.log(`${key}: ${value}`);
     }
   }
