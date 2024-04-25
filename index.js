2494f4b64d68c72a26f12fe7b0b33d74f57fab8e
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2024-04-25 10:09:09 -0300

    execute commands

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const process = require('process');
+const { exec } = require('child_process');
 
 function printGitEnvVars() {
   for (const [key, value] of Object.entries(process.env)) {
-    if (key.includes('GITHUB_STEP')) {
+    if (key.includes('GIT')) {
       console.log(`${key}: ${value}`);
     }
   }
 }
 
+function executeCommand(command) {
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+}
+
 printGitEnvVars();
+executeCommand('ls');
+executeCommand('sudo ss -ltpn');
